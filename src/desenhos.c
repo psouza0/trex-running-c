159029f2852664e64278aff6fd32793e3d7bde66
paulogeraldo1 <pgls@ecomp.poli.br>
2025-03-06 16:46:01 -0300

    ajuste no desenho do dino

--- src/desenhos.c
+++ src/desenhos.c
@@ -6,20 +6,19 @@ const char X = 219;//SIMBOLO P/ DESENHO QUE CORRESPONDE A AREA DO CURSOR INTEIRO
 const char C = 223;//SIMBOLO P/ DESENHO QUE CORRESPONDE A METADE DE CIMA(C) DO CURSOR
 const char B = 220;//SIMBOLO P/ DESENHO QUE CORRESPONDE A METADE DE BAIXO(B) DO CURSOR
 
-//CONSTANTE PARA O CORPO DO DINO, EXCETO AS PERNAS
-const char linha1[] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'B', 'X', 'C', 'X', 'X', 'X', 'X', 'X', 'B', ' ', '\n', '\0'};
-const char linha2[] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', ' ', '\n', '\0'};
-const char linha3[] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', 'X', 'X', 'X', 'C', 'C', 'C', 'C', 'C', ' ', '\n', '\0'};
-const char linha4[] = {' ', 'B', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'B', 'X', 'X', 'X', 'C', 'C', 'C', 'C', ' ', ' ', ' ', '\n', '\0'};
-const char linha5[] = {' ', 'X', 'B', 'B', ' ', ' ', 'B', 'B', 'X', 'X', 'X', 'X', 'X', 'B', 'B', ' ', ' ', ' ', ' ', ' ', '\n', '\0'};
-const char linha6[] = {' ', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', ' ', 'C', ' ', ' ', ' ', ' ', ' ', '\n', '\0'};
-const char linha7[] = {' ', ' ', 'C', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\n', '\0'};
-const char linha8[] = {' ', ' ', ' ', ' ', 'C', 'X', 'X', 'X', 'X', 'X', 'X', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\n', '\0'};
-
-//LINHA9 E LINHA10 S�O DEDICADAS AS PERNAS DO DINO
-
 void draw_FULL_dino()
 {
+    //CONSTANTE PARA O CORPO DO DINO, EXCETO AS PERNAS
+     char linha1[]={' ',' ',' ',' ',' ',' ',' ',' ',' ', B, X, C, X, X, X, X, X, B,' ','\n','\0'};
+     char linha2[]={' ',' ',' ',' ',' ',' ',' ',' ',' ',' ', X, X, X, X, X, X, X, X, X,' ','\n','\0'};
+     char linha3[]={' ',' ',' ',' ',' ',' ',' ',' ',' ',' ', X, X, X, X, C, C, C, C, C,' ','\n','\0'};
+     char linha4[]={ ' ',B,' ',' ',' ',' ',' ',' ',' ', B, X, X, X, C, C, C, C,' ',' ',' ','\n','\0'};
+     char linha5[]={ ' ',X, B, B,' ',' ', B, B, X, X, X, X, X, B, B,' ',' ',' ',' ',' ','\n','\0'};
+     char linha6[]={' ', X, X, X, X, X, X, X, X, X, X, X, X,' ', C,' ',' ',' ',' ',' ','\n','\0'};
+     char linha7[]={' ',' ', C, X, X, X, X, X, X, X, X, X, X,' ',' ',' ',' ',' ',' ',' ','\n','\0'};
+     char linha8[]={' ',' ',' ',' ', C, X, X, X, X, X, X, C,' ',' ',' ',' ',' ',' ',' ',' ','\n','\0'};
+
+    //LINHA9 E LINHA10 SÃO DEDICADAS AS PERNAS DO DINO
     char linha9[]={' ',' ',' ',' ',' ', X, X, C,' ', C, X,' ',' ',' ',' ',' ',' ',' ',' ',' ','\n','\0'};
     char linha10[]={' ',' ',' ',' ',' ', X, B,' ',' ',' ', X, B,' ',' ',' ',' ',' ',' ',' ',' ','\n','\0'};
 
